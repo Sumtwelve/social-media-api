14054d2060acb26bec70210fa6f67c818035f46d
sumtwelve <sumtwelve@gmail.com>
2023-04-22 23:18:30 -0600

    first draft of user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,17 +69,11 @@ module.exports = {
     async deleteUser(req, res) {
         try {
             const user = await User.findOneAndRemove({ _id: req.params.userId });
-
             if (!user) {
                 return res.status(404).json({ message: "No user found with that ID" });
             }
 
-            const thoughts = await Thought
-                .find({ userId: req.params.userId })
-                .remove((err) => err
-                                 ? console.error(err)
-                                 : console.log("Thoughts removed"));
-
+            const thoughts = await Thought.deleteMany({ _id: { $in: user.thoughts } });
             if (!thoughts) {
                 return res.status(404).json({ message: "User deleted, but no thoughts found to delete." });
             }
@@ -116,28 +110,15 @@ module.exports = {
     // DELETE to remove a friend from a user's friend list
     async removeFriend(req, res) {
         try {
-            // Find the friend
-            // FIXME: This implementation is slightly different from the above route.
-            // Here we find the friend separately, and then pass it directly into
-            // the update function. This isn't how I learned to do this sort of thing,
-            // but I want to test it and see if it works.
-            // If it does work, I would almost prefer this method, since it lets us
-            // return a separate error message if the friendId wasn't found
-            const friend = await User.findOne({ _id: req.params.friendId });
-            if (!friend) {
-                return res.status(404).json({ message: "Invalid Friend ID" });
-            }
-
-            // find the user
+            // Find the user
             const user = await User.findOneAndUpdate(
                 { _id: req.params.userId },
-                { $pull: { friends: friend } },
+                { $pull: { friends: { _id: req.params.friendId } } },
                 { new: true }
             );
             if (!user) {
                 return res.status(404).json({ message: "No user found with that ID" });
             }
-
             res.status(200).json(user);
         } catch (err) {
             res.status(500).json(err);
